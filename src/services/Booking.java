d0f3881c46fc5f6b46421977c27d2e14ef307779
chaitra2198 <bmchaitra2@gmail.com>
2020-08-06 14:55:36 +0530

    FSD java d3

--- src/services/Booking.java
+++ src/services/Booking.java
@@ -2,3 +2,17 @@ package services;
 
 // Type your code
 
+//parent class
+public class Booking{
+	
+	//method returning rates
+	public double booking(int noOfPersons,int rates)
+	{
+	
+        double d=(double)noOfPersons*rates;
+        return d;
+		
+}
+	
+	
+}
